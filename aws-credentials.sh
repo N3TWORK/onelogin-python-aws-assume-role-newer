6e873a25b7afd2d1cc9329920be5d76ac1004775
Felipe Apablaza <fapablaza@n3twork.com>
2023-03-24 16:30:32 -0300

    fixed repeated python configuration code

--- aws-credentials.sh
+++ aws-credentials.sh
@@ -13,8 +13,18 @@ function is_valid_email() {
 
 EMAIL=$1
 SCRIPT_DIR=$(dirname -- "$0")
+VENV_DIR="venv"
 cd $SCRIPT_DIR
 ONELOGIN_SDK_JSON="onelogin.sdk.json"
+
+# configures local python environment
+if [[ -d $VENV_DIR ]]; then
+  source venv/bin/activate
+else
+  python3 -m venv venv
+  source venv/bin/activate
+  pip3 install -r requirements.txt
+fi
 
 # checks for the developer emails
 if ! is_valid_email "$EMAIL"; then
@@ -27,18 +37,6 @@ if [[ ! -f $ONELOGIN_SDK_JSON ]]; then
   echo "Error: onelogin.sdk.json file not found in the script directory. Please create one at this (relative) path: ./$SCRIPT_DIR"
   exit 1
 fi
-
-if [[ -d $VENV_DIR ]]; then
-  source venv/bin/activate
-else
-  python3 -m venv venv
-  source venv/bin/activate
-  pip3 install -r requirements.txt
-fi
-
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
 
 # for output in stdout
 python3 src/onelogin/aws-assume-role/aws-assume-role.py -a 843789 -d n3twork -z 43200 -u $EMAIL --aws-region us-east-1 --onelogin-password $(security find-internet-password -a $EMAIL -s onelogin -w) --role_order
